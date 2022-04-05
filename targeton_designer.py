984243550a00ae574dda55ceda9df9d8d40807b1
pk8 <pk8@sanger.ac.uk>
2022-04-05 10:53:22 +0000

    Hashified primer3 output

--- targeton_designer.py
+++ targeton_designer.py
@@ -4,8 +4,9 @@ from flask import Flask
 app = Flask(__name__)
 cache = redis.Redis(host='redis', port=6379)
 
-from runner.spawner import primer3_runner
+from runner.primer3 import primer3_runner
 
 @app.route('/')
-def hello():
-    return primer3_runner()
+def start():
+    primers = primer3_runner()
+    return primers   
