8996e62de12711afc91ce14b5846e7149f4e1816
pk8 <pk8@sanger.ac.uk>
2022-04-20 13:20:11 +0000

    Move seq input into param

--- runner/primer3.py
+++ runner/primer3.py
@@ -5,23 +5,19 @@ import re
 import os
 import collections
 
-def primer3_runner():
-    design = primer3_design()
+def primer3_runner(design_input):
+    design = primer3_design(design_input)
     primers = locate_primers(design)
     return primers
 
-def primer3_design():
+def primer3_design(primer3_input):
     p3_config_loc = os.environ.get('PRIMER3_CONFIG')
-    print(p3_config_loc,flush=True)
+    
     primer3_config = {}
     with open(os.path.join(os.path.dirname(__file__), p3_config_loc), "r+") as p3:
         primer3_config = json.load(p3)
 
-    design = primer3.bindings.designPrimers({
-        'SEQUENCE_ID': 'MH1000',
-        'SEQUENCE_TEMPLATE': 'GCTTGCATGCCTGCAGGTCGACTCTAGAGGATCCCCCTACATTTTAGCATCAGTGAGTACAGCATGCTTACTGGAAGAGAGGGTCATGCAACAGATTAGGAGGTAAGTTTGCAAAGGCAGGCTAAGGAGGAGACGCACTGAATGCCATGGTAAGAACTCTGGACATAAAAATATTGGAAGTTGTTGAGCAAGTNAAAAAAATGTTTGGAAGTGTTACTTTAGCAATGGCAAGAATGATAGTATGGAATAGATTGGCAGAATGAAGGCAAAATGATTAGACATATTGCATTAAGGTAAAAAATGATAACTGAAGAATTATGTGCCACACTTATTAATAAGAAAGAATATGTGAACCTTGCAGATGTTTCCCTCTAGTAG',
-        'SEQUENCE_INCLUDED_REGION': [36,342]
-    }, primer3_config)
+    design = primer3.bindings.designPrimers(primer3_input, primer3_config)
 
     return design
 
