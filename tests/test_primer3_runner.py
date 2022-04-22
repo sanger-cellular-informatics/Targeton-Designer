0b51117a8957ebd4a405c23a4b53d6ab28de1890
pk8 <pk8@sanger.ac.uk>
2022-04-22 11:03:28 +0000

    Swapped for assertEqual

--- tests/test_primer3_runner.py
+++ tests/test_primer3_runner.py
@@ -4,7 +4,6 @@ import os
 from runner.primer3 import primer3_runner
 
 class TestRunner(unittest.TestCase):
-    @classmethod
     def test_runner(self):
         os.environ["PRIMER3_CONFIG"] = "../tests/primer3_test_config.json"
 
@@ -14,9 +13,8 @@ class TestRunner(unittest.TestCase):
         }
 
         result = primer3_runner(design_input)
-        self.assertTrue(result['primer_left_0']['sequence'], 'TCCACACAGGATGCCAGG')
-
-        os.environ["PRIMER3_CONFIG"] = ""
+        self.assertEqual(result['primer_left_0']['sequence'], 'TCCACACAGGATGCCAGG', 'Left primer ok')
+        self.assertEqual(result['primer_right_0']['sequence'], 'GGACACTCACCTCAGTTCCTG', 'Right primer ok')
 
 if __name__ == '__main__':
     unittest.main()
