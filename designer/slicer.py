3cfc6e5b8a349092b6ce58a999cff20bd449c13b
Beth Sampher <bs15@sanger.ac.uk>
2022-06-13 13:42:38 +0100

    Added count for slice naming

--- designer/slicer.py
+++ designer/slicer.py
@@ -3,22 +3,26 @@ from pybedtools import BedTool
 import argparse
 import sys
 
-def _generate_slice_data(exon, count, params):
+def _generate_slice_data(exon, exon_name, params):
     slices = []
-    name = exon.name if exon.name != '.' else count
     start = exon.start - params['flank_5']
     end = start + params['length']
+    count = 1
     while end <= (exon.end + params['flank_3']):
-        slices.append((exon.chrom, start, end, name, exon.score, exon.strand))
+        slice_name = f'{exon_name}_{count}'
+        slices.append((exon.chrom, start, end, slice_name,
+            exon.score, exon.strand))
         start += params['offset']
         end += params['offset']
+        count += 1
     return slices
 
 def get_slice_data(bed, params):
     slices = []
     count = 1
     for exon in bed:
-        slices.extend(_generate_slice_data(exon, count, params))
+        name = exon.name if exon.name != '.' else f'region{count}'
+        slices.extend(_generate_slice_data(exon, name, params))
         count += 1
     return slices
 
@@ -31,38 +35,38 @@ def parse_args(args):
     parser = argparse.ArgumentParser(
         description='Get sequence slices for regions in'\
             ' BED file according to parameters specified')
-    parser.add_argument('bed',
+    parser.add_argument('input_bed',
         help='BED file containing regions of interest')
-    parser.add_argument('fasta',
+    parser.add_argument('input_fasta',
         help='FASTA file to retrieve sequences from')
     parser.add_argument('-f5', '--flank_5',
-        help='how far to extend region at 5\' end',
+        help='how far to extend region at 5\' end (default 50nt)',
         type=int, default=50)
     parser.add_argument('-f3', '--flank_3',
-        help='how far to extend region at 3\' end',
+        help='how far to extend region at 3\' end (default 50nt)',
         type=int, default=50)
     parser.add_argument('-l', '--length',
-        help='length of each slice',
+        help='length of each slice (default 210nt)',
         type=positive_int, default=210)
     parser.add_argument('-o', '--offset',
-        help='offset between each slice',
+        help='offset between each slice (default 5nt)',
         type=positive_int, default=5)
     parser.add_argument('--output_fasta',
         help='output slice sequences to fasta file')
-    parser.add_argument('--output_slice_bed',
+    parser.add_argument('--output_bed',
         help='output bed file with slice coordinates')
     return parser.parse_args(args)
 
 def get_slices(params):
-    bed = BedTool(params['bed'])
+    bed = BedTool(params['input_bed'])
     slice_bed = BedTool(get_slice_data(bed, params))
     # return named slice sequences on specified strand
-    return slice_bed.sequence(fi=params['fasta'], name=True, s=True)
+    return slice_bed.sequence(fi=params['input_fasta'], name=True, s=True)
 
 def main(params):
     slices = get_slices(params)
-    if params['output_slice_bed'] is not None:
-        slices.saveas(params['output_slice_bed'])
+    if params['output_bed'] is not None:
+        slices.saveas(params['output_bed'])
     if params['output_fasta'] is not None:
         slices.save_seqs(params['output_fasta'])
         print('Slice sequences saved!')
