3f4d5b22712b9b2ce4f8e53cb76257e405f3486b
pk8 <pk8@sanger.ac.uk>
2022-07-18 15:18:46 +0000

    Boolean pretty test value

--- tests/ipcress/test_ipcress.py
+++ tests/ipcress/test_ipcress.py
@@ -7,7 +7,7 @@ from src.ipcress.ipcress import (prettify_output)
 class TestSlicer(TestCase):
     def test_prettify_output_defined(self):
         expected = 'test_cmd --pretty true'
-        prettify_param = 'true'
+        prettify_param = True
         cmd = 'test_cmd'
         
         actual = prettify_output(prettify_param, cmd)
