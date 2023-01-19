bc6b41830724164ac6f03327ac930b8b124035a0
Peter Inzani <pi2@sanger.ac.uk>
2023-01-19 17:00:00 +0000

    Cleaned up test_integration of commented out lines and added __init__.py to the folder. Removed output files that weren't needed. Modified the tests folder __init__.py to remove the suite function as it wasn't ever run and contained reference to a very old test library. Restored the CI to main, but changed the run condition to be on any push, rather than only on pull request.

--- tests/__init__.py
+++ tests/__init__.py
@@ -4,7 +4,3 @@ import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
 
 import src
-
-def suite():   
-    return unittest.TestLoader().discover("satlib_app.tests", pattern="*.py")
-
