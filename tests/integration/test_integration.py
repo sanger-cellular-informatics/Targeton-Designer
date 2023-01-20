ff3937d892037ed2bfaba64bfb167f26e00abbf1
Peter Inzani <pi2@sanger.ac.uk>
2023-01-20 14:17:25 +0000

    Linted test_integration.py according to pep8 and added max-line-length = 100 to setup.cfg.

--- tests/integration/test_integration.py
+++ tests/integration/test_integration.py
@@ -17,22 +17,21 @@ class TestSlicerIntegration(TestCase):
         self.bed_file_path = r"./tests/integration/fixtures/bed_example.bed"
         self.fasta_file_path = r"./tests/integration/fixtures/fasta_example.fa"
 
-    def test_SlicerOutput(self):  
+    def test_SlicerOutput(self):
         with TemporaryDirectory() as tmpdir:
             # Use unittest patch to mock sys.argv as if gived the commands listed via CLI.
-            with patch.object(sys,'argv',["./designer.sh","slicer", "--bed", self.bed_file_path, "--fasta", self.fasta_file_path,"--dir",tmpdir]):
+            with patch.object(sys, 'argv', ["./designer.sh", "slicer", "--bed", self.bed_file_path, "--fasta", self.fasta_file_path, "--dir", tmpdir]):
                 parsed_input = ParsedInputArguments()
                 args = parsed_input.get_args()
-                slicer_result=slicer_command(args)
-                path_bed=Path(slicer_result.bed)
-                path_fasta=Path(slicer_result.fasta)
+                slicer_result = slicer_command(args)
+                path_bed = Path(slicer_result.bed)
+                path_fasta = Path(slicer_result.fasta)
                 # # Check if the files exist.
                 self.assertTrue(path_bed.is_file())
                 self.assertTrue(path_fasta.is_file())
                 # # Check if the files are empty
-                self.assertGreater(path_bed.stat().st_size,0)
-                self.assertGreater(path_fasta.stat().st_size,0)
-        
+                self.assertGreater(path_bed.stat().st_size, 0)
+                self.assertGreater(path_fasta.stat().st_size, 0)
 
 
 if __name__ == '__main__':
