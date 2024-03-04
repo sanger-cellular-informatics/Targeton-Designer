adf2c3dbd2e088efe574fee2d776b97f7cb2ea72
Anna Shtanova <as63@sanger.ac.uk>
2024-03-04 15:42:11 +0000

    TD-528 Fix kmer list file name

--- download_kmer_lists.sh
+++ download_kmer_lists.sh
@@ -3,6 +3,6 @@
 mkdir kmer
 
 curl -o kmer/homo_sapiens_16.list https://primer3.ut.ee/lists/homo_sapiens_16.list
-curl -o kmer/homo_sapiens_16.list https://primer3.ut.ee/lists/homo_sapiens_11.list
+curl -o kmer/homo_sapiens_11.list https://primer3.ut.ee/lists/homo_sapiens_11.list
 
 ls kmer
