a0a363e715ac1795aa26cd5366bd66b2015870ea
Miguel Rada <mr33@sanger.ac.uk>
2024-03-14 10:33:01 +0000

    TD-545/HAP1-variant-primer-filter

--- tests/primer/filter/test_hap1.py
+++ tests/primer/filter/test_hap1.py
@@ -6,41 +6,27 @@ import requests
 from primer.filter.hap1 import contain_variant
 
 
-class TestPrimer3(TestCase):
+class TestHap1(TestCase):
 
     @patch('requests.get')
-    def test_contain_variant_with_variants(self, mock_get):
-        mock_response = Mock()
-        mock_response.status_code = 200
-        mock_response.text = '{"variants": ["variant1", "variant2"]}'
-        mock_get.return_value = mock_response
+    def test_contain_variant_with_variants(self, request_get):
+        http_response = Mock(status_code=200, text='{"variants": ["variant1", "variant2"]}')
 
-        chromosome = "chr1"
-        start = 100
-        end = 200
+        request_get.return_value = http_response
 
-        self.assertTrue(contain_variant(chromosome, start, end))
+        self.assertTrue(contain_variant(chromosome="chr1", start=100, end=200))
 
     @patch('requests.get')
-    def test_contain_variant_without_variants(self, mock_get):
-        mock_response = Mock()
-        mock_response.status_code = 200
-        mock_response.text = '{"variants": []}'
-        mock_get.return_value = mock_response
+    def test_contain_variant_without_variants(self, request_get):
+        http_response = Mock(status_code=200, text='{"variants": []}')
 
-        chromosome = "chr1"
-        start = 100
-        end = 200
+        request_get.return_value = http_response
 
-        self.assertFalse(contain_variant(chromosome, start, end))
+        self.assertFalse(contain_variant(chromosome="chr1", start=100, end=200))
 
     @patch('requests.get')
     def test_contain_variant_request_exception(self, mock_get):
         mock_get.side_effect = requests.exceptions.RequestException
 
-        chromosome = "chr1"
-        start = 100
-        end = 200
-
         with self.assertRaises(requests.exceptions.RequestException):
-            contain_variant(chromosome, start, end)
+            contain_variant(chromosome="chr1", start=100, end=200)
