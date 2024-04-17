aba2bf0034a7ccbc28c739ee281f84218e2ca10f
Miguel Rada <mr33@sanger.ac.uk>
2024-04-17 14:50:26 +0100

    Code review changes: Add primer3 integration test

--- tests/primer/test_primer3.py
+++ tests/primer/test_primer3.py
@@ -77,6 +77,7 @@ class IntegrationTestPrimer3(TestCase):
         self.setUpPyfakefs()
 
     def test_get_primer_pairs_from_fasta_file(self):
+        stringency = 1
         chromosome = "1"
         pre_targeton_name = "ENSE00000769557_HG8_11"
         pre_targeton_start = 42929593
@@ -87,7 +88,7 @@ class IntegrationTestPrimer3(TestCase):
             'fasta.fa',
             contents=f'>{pre_targeton_name}::{chromosome}:{pre_targeton_start}-{pre_targeton_end}(-)\nCACCTTCCCTCCGGTCCCCCCAGTGCTAAAGAAGCTGCGCGGGACAGCTGACGTGACCCATGACCTGCAGGAGATGAAGGAAGAGAGTCGGCAGATGATGCGGGAGAAGAAGGTCACCATCCTGGAGCTGTTCCGCTCCCCCGCCTACCGCCAGCCCATCCTCATCGCTGTGGTGCTGCAGCTGTCCCAGCAGCTGTCTGGCATCAACGC')
 
-        designer_config = {"stringency_vector": [""]}
+        designer_config = {"stringency_vector": [stringency]}
 
         p3_config = {
             "PRIMER_TASK": "pick_cloning_primers",
@@ -101,32 +102,32 @@ class IntegrationTestPrimer3(TestCase):
             "PRIMER_EXPLAIN_FLAG": 1
         }
 
+        # act
+        result = Primer3(designer_config, p3_config).get_primers(slices_fasta_file.name)
+
+        # assert
         expected_primer_pair = PrimerPair(
-            pair_id=f'{pre_targeton_name}_0_str',
+            pair_id=f'{pre_targeton_name}_0_str1',
             chromosome=chromosome,
             pre_targeton_start=f'{pre_targeton_start}',
             pre_targeton_end=f'{pre_targeton_end}'
         )
         expected_forward = {'primer': f'{pre_targeton_name}_LibAmpF_0', 'penalty': 2.7456977357412597,
-                            'side': 'right',
-                            'pair_id': f'{pre_targeton_name}_0_str', 'sequence': 'CAGACAGCTGCTGGGACA',
+                            'side': 'right', 'stringency': f'{stringency}',
+                            'pair_id': f'{pre_targeton_name}_0_str{stringency}', 'sequence': 'CAGACAGCTGCTGGGACA',
                             'coords': [199, 18],
                             'primer_start': 42929775, 'primer_end': 42929793, 'strand': '+', 'tm': 59.25430226425874,
                             'gc_percent': 61.111111111111114, 'self_any_th': 30.996860910464648, 'self_end_th': 0.0,
                             'hairpin_th': 35.513327628973116, 'end_stability': 4.02}
         expected_reverse = {'primer': f'{pre_targeton_name}_LibAmpR_0', 'penalty': 3.400054355094312, 'side': 'left',
-                            'pair_id': f'{pre_targeton_name}_0_str', 'sequence': 'CACCTTCCCTCCGGTCCC',
-                            'coords': [0, 18],
+                            'stringency': f'{stringency}', 'pair_id': f'{pre_targeton_name}_0_str{stringency}',
+                            'sequence': 'CACCTTCCCTCCGGTCCC', 'coords': [0, 18],
                             'primer_start': 42929593, 'primer_end': 42929611, 'strand': '-', 'tm': 61.40005435509431,
                             'gc_percent': 72.22222222222223, 'self_any_th': 0.0, 'self_end_th': 0.0, 'hairpin_th': 0.0,
                             'end_stability': 4.46}
         expected_primer_pair.forward = expected_forward
         expected_primer_pair.reverse = expected_reverse
 
-        # act
-        result = Primer3(designer_config, p3_config).get_primers(slices_fasta_file.name)
-
-        # assert
         self.assertEqual(result, [expected_primer_pair])
 
 
