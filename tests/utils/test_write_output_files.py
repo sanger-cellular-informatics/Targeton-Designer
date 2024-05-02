2c17d04947cbfb418c87bec9e50c3cd3bfe7f56f
eh21 <eh21@sanger.ac.uk>
2024-05-02 15:22:45 +0000

    Move tests for the write_primer_output.py _reorder_columns function to right test file

--- tests/utils/test_write_output_files.py
+++ tests/utils/test_write_output_files.py
@@ -3,14 +3,12 @@ from io import StringIO
 from os import path
 from pathlib import Path
 import csv
-import sys
 
 import pandas as pd
 from pyfakefs.fake_filesystem_unittest import TestCase
 from unittest.mock import patch, Mock
 from freezegun import freeze_time
 
-from primer.write_primer_output import _reorder_columns
 from src.utils.write_output_files import write_scoring_output, write_targeton_csv
 from src.utils import write_output_files
 from primer.slice_data import SliceData
@@ -136,129 +134,6 @@ class TestWriteOutputFiles(TestCase):
         self.assertTrue(expected_file_path.exists())
         self.assertEqual(test_data, expected_read_data)
 
-
-    def test_reorder_columns_when_duplicate_column_names(self):
-
-        # Arrange
-        column_names = ['Name', 'Age', 'Name']
-        data = {
-            'Name': ['Juan', 'Maria', 'Pedro'],
-            'Age': [25, 30, 35]
-        }
-        df = pd.DataFrame(data)
-
-        # Act
-        ordered_df = _reorder_columns(column_names, df)
-
-        # Assertion
-        pd.testing.assert_frame_equal(ordered_df, df)
-
-
-    def test_reorder_columns_when_empty_column_names(self):
-        expected_stdout = StringIO()
-        sys.stdout = expected_stdout
-
-        # Arrange
-        column_names = []
-        data = {
-            'Name': ['Juan', 'Maria', 'Pedro'],
-            'Age': [25, 30, 35]
-        }
-        df = pd.DataFrame(data)
-
-        # Act
-        ordered_df = _reorder_columns(column_names, df)
-
-        # Assertion
-        std_result = expected_stdout.getvalue().strip()
-
-        pd.testing.assert_frame_equal(ordered_df, df)
-        self.assertEqual(std_result, 
-                         "Warning: empty csv_column_order list provided in config file, returning dataframe with default column order")
-
-
-    def test_reorder_columns_when_all_column_names_wrong(self):
-        expected_stdout = StringIO()
-        sys.stdout = expected_stdout
-
-        # Arrange
-        column_names = ['WRONG']
-        data = {
-            'Name': ['Juan', 'Maria', 'Pedro'],
-            'Age': [25, 30, 35]
-        }
-        df = pd.DataFrame(data)
-
-        # Act
-        with self.assertRaises(ValueError) as value_error:
-            _reorder_columns(column_names, df)
-
-        # Assertion
-        std_result = expected_stdout.getvalue().strip()
-
-        self.assertEqual(str(value_error.exception), "All column names in config file are wrong")
-        self.assertEqual(std_result, "Warning: 'WRONG' specified in config file not is not a column name")
-    
-
-    def test_reorder_columns_when_some_column_names_wrong(self):
-        expected_stdout = StringIO()
-        sys.stdout = expected_stdout
-
-        # Arrange
-        column_names = ['WRONG', 'Name', 'Age']
-        data = {
-            'Name': ['Juan', 'Maria', 'Pedro'],
-            'Age': [25, 30, 35]
-        }
-        df = pd.DataFrame(data)
-
-        # Act
-        ordered_df = _reorder_columns(column_names, df)
-
-        # Assertion
-        std_result = expected_stdout.getvalue().strip()
-
-        pd.testing.assert_frame_equal(ordered_df, df[['Name', 'Age']])
-        self.assertEqual(std_result, "Warning: 'WRONG' specified in config file not is not a column name")
-
-
-    def test_reorder_columns_when_some_column_names_missing(self):
-        expected_stdout = StringIO()
-        sys.stdout = expected_stdout
-
-        # Arrange
-        column_names = ['Name']
-        data = {
-            'Name': ['Juan', 'Maria', 'Pedro'],
-            'Age': [25, 30, 35]
-        }
-        df = pd.DataFrame(data)
-
-        # Act
-        ordered_df = _reorder_columns(column_names, df)
-
-        # Assertion
-        std_result = expected_stdout.getvalue().strip()
-
-        pd.testing.assert_frame_equal(ordered_df, df[['Name']])
-        self.assertEqual(std_result, "'Age' column discarded as it is not in config file")
-
-
-    def test_reorder_columns_success(self):
-
-        # Arrange
-        column_names = ['Age', 'Name']
-        data = {
-            'Name': ['Juan', 'Maria', 'Pedro'],
-            'Age': [25, 30, 35]
-        }
-        df = pd.DataFrame(data)
-
-        # Act
-        ordered_df = _reorder_columns(column_names, df)
-
-        # Assertion
-        pd.testing.assert_frame_equal(ordered_df, df[['Age', 'Name']])
 
 
 if __name__ == '__main__':
