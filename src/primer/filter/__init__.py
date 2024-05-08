5e501c3008b0e05aa79031d0aa61afd2fbed08b4
Miguel Rada <mr33@sanger.ac.uk>
2024-05-08 10:50:25 +0100

    Add full package definition on filter init

--- src/primer/filter/__init__.py
+++ src/primer/filter/__init__.py
@@ -1,3 +1,3 @@
-from .duplicates_filter import DuplicatesFilter
-from .hap1_variant_filter import HAP1VariantFilter
-from .filter import Filter
+from primer.filter.duplicates_filter import DuplicatesFilter
+from primer.filter.hap1_variant_filter import HAP1VariantFilter
+from primer.filter.filter import Filter
