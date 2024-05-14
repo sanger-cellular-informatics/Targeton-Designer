2a417d41f4b1b527755897ffec8eb620e59f5d17
Helen Schuilenburg <hs21@sanger.ac.uk>
2024-05-14 17:01:05 +0000

    TD-601 Add tests for _add_primer_pair

--- tests/primer/test_write_primer_output.py
+++ tests/primer/test_write_primer_output.py
@@ -6,8 +6,10 @@ from mock import patch
 import pandas as pd
 from pyfakefs.fake_filesystem_unittest import TestCase
 
-
-from primer.write_primer_output import _reorder_columns
+from collections import defaultdict
+from primer.primer_pair import PrimerPair
+from primer.designed_primer import DesignedPrimer, Interval
+from primer.write_primer_output import _reorder_columns, _add_primer_pair
 
 class CapturingStreamHandler(logging.StreamHandler):
   """Custom StreamHandler to capture logs in memory."""
@@ -138,6 +140,182 @@ class TestWritePrimerOutputFiles(TestCase):
 
             # Assertion
             pd.testing.assert_frame_equal(ordered_df, df[['Age', 'Name']])
+
+
+class TestDataFrameBuild(TestCase):
+    def test__add_primer_pair_when_dict_empty(self):
+        test_dict = defaultdict(list)
+        primer_type = "LibAmp"
+
+        pair = PrimerPair(
+                pair_id="ABCD_0_str1",
+                chromosome="1",
+                pre_targeton_start=42930996,
+                pre_targeton_end=42931206,
+                product_size="129",
+                stringency=1,
+                targeton_id="ABCD",
+                uid="uid0")
+
+        pair.forward = DesignedPrimer(
+                name="ABCD_LibAmpF_0",
+                penalty=0.16,
+                pair_id="ABCD_0_str1",
+                sequence="AGAAAACGCTGGTGTGGTGA",
+                coords=Interval(start=169, end=20),
+                primer_start=42931146,
+                primer_end=42931166,
+                strand="+",
+                tm=60.0,
+                gc_percent=50.0,
+                self_any_th=0.0,
+                self_end_th=0.0,
+                hairpin_th=0.0,
+                end_stability=4.02
+            )
+
+        pair.reverse = DesignedPrimer(
+                name="ABCD_LibAmpR_0",
+                penalty=0.30,
+                pair_id="ABCD_0_str1",
+                sequence="TGTTGCTCTTTTCCCAGGCT",
+                coords=Interval(start=41, end=20),
+                primer_start=42930996,
+                primer_end=42931016,
+                strand="-",
+                tm=59.8,
+                gc_percent=50.1,
+                self_any_th=0.1,
+                self_end_th=0.2,
+                hairpin_th=0.2,
+                end_stability=4.58
+            )
+
+        expected_dict = defaultdict(list)
+        expected_dict = {
+                'primer_type': ['LibAmp', 'LibAmp'],
+                'primer': ['ABCD_LibAmpF_0', 'ABCD_LibAmpR_0'],
+                'penalty': [0.16, 0.3],
+                'sequence': ['AGAAAACGCTGGTGTGGTGA', 'TGTTGCTCTTTTCCCAGGCT'],
+                'primer_start': [42931146, 42930996],
+                'primer_end': [42931166, 42931016],
+                'tm': [60.0, 59.8],
+                'gc_percent': [50.0, 50.1],
+                'self_any_th': [0.0, 0.1],
+                'self_end_th': [0.0, 0.2],
+                'hairpin_th': [0.0, 0.2],
+                'end_stability': [4.02, 4.58],
+                'pair_uid': ['uid0', 'uid0'],
+                'stringency': [1, 1],
+                'chromosome': ['1', '1'],
+                'pre_targeton_start': [42930996, 42930996],
+                'pre_targeton_end': [42931206, 42931206],
+                'product_size': ['129', '129'],
+                'targeton_id': ['ABCD', 'ABCD']
+                }
+
+        self.maxDiff = None
+        self.assertIsNone(_add_primer_pair(test_dict, pair, primer_type))
+        self.assertDictEqual(test_dict, expected_dict)
+
+    def test__add_primer_pair_when_dict_not_empty(self):
+        test_dict = defaultdict(list)
+        primer_type = "LibAmp"
+
+        test_dict = {
+                'primer_type': ['LibAmp', 'LibAmp'],
+                'primer': ['ABCD_LibAmpF_0', 'ABCD_LibAmpR_0'],
+                'penalty': [0.16, 0.3],
+                'sequence': ['AGAAAACGCTGGTGTGGTGA', 'TGTTGCTCTTTTCCCAGGCT'],
+                'primer_start': [42931146, 42930996],
+                'primer_end': [42931166, 42931016],
+                'tm': [60.0, 59.8],
+                'gc_percent': [50.0, 50.1],
+                'self_any_th': [0.0, 0.1],
+                'self_end_th': [0.0, 0.2],
+                'hairpin_th': [0.0, 0.2],
+                'end_stability': [4.02, 4.58],
+                'pair_uid': ['uid0', 'uid0'],
+                'stringency': [1, 1],
+                'chromosome': ['1', '1'],
+                'pre_targeton_start': [42930996, 42930996],
+                'pre_targeton_end': [42931206, 42931206],
+                'product_size': ['129', '129'],
+                'targeton_id': ['ABCD', 'ABCD']
+            }
+
+        pair = PrimerPair(
+                pair_id="ABCD_1_str1",
+                chromosome="1",
+                pre_targeton_start=42930996,
+                pre_targeton_end=42931206,
+                product_size="122",
+                stringency=1,
+                targeton_id="ABCD",
+                uid="uid1")
+
+        pair.forward = DesignedPrimer(
+                name="ABCD_LibAmpF_1",
+                penalty=0.17,
+                pair_id="ABCD_1_str1",
+                sequence="GCTGGTGTGGTGATATGCCT",
+                coords=Interval(start=169, end=20),
+                primer_start=42931139,
+                primer_end=42931159,
+                strand="+",
+                tm=60.1,
+                gc_percent=50.2,
+                self_any_th=0.2,
+                self_end_th=0.3,
+                hairpin_th=0.3,
+                end_stability=4.75
+            )
+
+        pair.reverse = DesignedPrimer(
+                name="ABCD_LibAmpR_1",
+                penalty=0.31,
+                pair_id="ABCD_1_str1",
+                sequence="TGTTGCTCTTTTCCCAGGCT",
+                coords=Interval(start=41, end=20),
+                primer_start=42930996,
+                primer_end=42931016,
+                strand="-",
+                tm=59.9,
+                gc_percent=50.3,
+                self_any_th=0.3,
+                self_end_th=0.4,
+                hairpin_th=0.4,
+                end_stability=4.58
+            )
+
+        expected_dict = defaultdict(list)
+        expected_dict = {
+                'primer_type': ['LibAmp', 'LibAmp', 'LibAmp', 'LibAmp'],
+                'primer': ['ABCD_LibAmpF_0', 'ABCD_LibAmpR_0',
+                           'ABCD_LibAmpF_1', 'ABCD_LibAmpR_1'],
+                'penalty': [0.16, 0.3, 0.17, 0.31],
+                'sequence': ['AGAAAACGCTGGTGTGGTGA', 'TGTTGCTCTTTTCCCAGGCT',
+                             'GCTGGTGTGGTGATATGCCT', 'TGTTGCTCTTTTCCCAGGCT'],
+                'primer_start': [42931146, 42930996, 42931139, 42930996],
+                'primer_end': [42931166, 42931016, 42931159, 42931016],
+                'tm': [60.0, 59.8, 60.1, 59.9],
+                'gc_percent': [50.0, 50.1, 50.2, 50.3],
+                'self_any_th': [0.0, 0.1, 0.2, 0.3],
+                'self_end_th': [0.0, 0.2, 0.3, 0.4],
+                'hairpin_th': [0.0, 0.2, 0.3, 0.4],
+                'end_stability': [4.02, 4.58, 4.75, 4.58],
+                'pair_uid': ['uid0', 'uid0', 'uid1', 'uid1'],
+                'stringency': [1, 1, 1, 1],
+                'chromosome': ['1', '1', '1', '1'],
+                'pre_targeton_start': [42930996, 42930996, 42930996, 42930996],
+                'pre_targeton_end': [42931206, 42931206, 42931206, 42931206],
+                'product_size': ['129', '129', '122', '122'],
+                'targeton_id': ['ABCD', 'ABCD', 'ABCD', 'ABCD']
+            }
+
+        self.maxDiff = None
+        self.assertIsNone(_add_primer_pair(test_dict, pair, primer_type))
+        self.assertDictEqual(test_dict, expected_dict)
 
 
 if __name__ == '__main__':
