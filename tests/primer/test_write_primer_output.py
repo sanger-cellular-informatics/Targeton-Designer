99bd9a594cd6c20a671a0144d39f88241b338e9c
Miguel Rada <mr33@sanger.ac.uk>
2024-06-06 17:09:04 +0100

    TD-640/Write optimal primer output file function and tests

--- tests/primer/test_write_primer_output.py
+++ tests/primer/test_write_primer_output.py
@@ -1,6 +1,8 @@
 import logging
 import unittest
-from io import StringIO
+from os import path
+from unittest.mock import patch
+
 import pandas as pd
 from pyfakefs.fake_filesystem_unittest import TestCase
 
@@ -9,128 +11,147 @@ from tests.utils.utils import CapturingStreamHandler
 from collections import defaultdict
 from primer.primer_pair import PrimerPair
 from primer.designed_primer import DesignedPrimer, Interval
-from primer.write_primer_output import _reorder_columns, _add_primer_pair
+from primer.write_primer_output import _reorder_columns, _add_primer_pair, export_three_optimal_primer_pairs_to_csv
 
 
 class TestWritePrimerOutputFiles(TestCase):
-        
-        def setUp(self):
-            # Create a custom stream handler to capture logs
-            self.handler = CapturingStreamHandler()
-            self.logger = self.handler.get_logger(self.handler)
+    def setUp(self):
+        # Create a custom stream handler to capture logs
+        self.handler = CapturingStreamHandler()
+        self.logger = self.handler.get_logger(self.handler)
 
-        def tearDown(self):
-            # Remove the handler after each test to reset logging
-            logger = logging.getLogger()
-            logger.removeHandler(self.handler)
-    
-        def test_reorder_columns_when_duplicate_column_names(self):
+        self.setUpPyfakefs()
 
-            # Arrange list indicating column order (contains duplicates) and dataframe for reordering
-            column_names = ['Name', 'Age', 'Name']
-            data = {
-                'Name': ['Juan', 'Maria', 'Pedro'],
-                'Age': [25, 30, 35]
-            }
-            df = pd.DataFrame(data)
+    def tearDown(self):
+        # Remove the handler after each test to reset logging
+        logger = logging.getLogger()
+        logger.removeHandler(self.handler)
 
-            # Act (reorder dataframe according to list)
-            ordered_df = _reorder_columns(column_names, df)
+    def test_reorder_columns_when_duplicate_column_names(self):
+        # Arrange list indicating column order (contains duplicates) and dataframe for reordering
+        column_names = ['Name', 'Age', 'Name']
+        data = {
+            'Name': ['Juan', 'Maria', 'Pedro'],
+            'Age': [25, 30, 35]
+        }
+        df = pd.DataFrame(data)
 
-            # Assertion
-            pd.testing.assert_frame_equal(ordered_df, df)
-            logs = self.handler.buffer.getvalue().strip()
-            self.assertEqual(logs, "'Name' duplicated in config file, only first instance retained")
-        
-    
-        def test_reorder_columns_when_empty_column_names(self):
+        # Act (reorder dataframe according to list)
+        ordered_df = _reorder_columns(column_names, df)
 
-            # Arrange list indicating column order (empty list) and dataframe for reordering
-            column_names = []
-            data = {
-                'Name': ['Juan', 'Maria', 'Pedro'],
-                'Age': [25, 30, 35]
-            }
-            df = pd.DataFrame(data)
+        # Assertion
+        pd.testing.assert_frame_equal(ordered_df, df)
+        logs = self.handler.buffer.getvalue().strip()
+        self.assertEqual(logs, "'Name' duplicated in config file, only first instance retained")
 
-            ordered_df = _reorder_columns(column_names, df)
+    def test_reorder_columns_when_empty_column_names(self):
+        # Arrange list indicating column order (empty list) and dataframe for reordering
+        column_names = []
+        data = {
+            'Name': ['Juan', 'Maria', 'Pedro'],
+            'Age': [25, 30, 35]
+        }
+        df = pd.DataFrame(data)
 
-            pd.testing.assert_frame_equal(ordered_df, df)
-            logs = self.handler.buffer.getvalue().strip()
-            self.assertEqual(logs, "Empty csv_column_order list provided in config file, returning dataframe with default column order")
+        ordered_df = _reorder_columns(column_names, df)
 
+        pd.testing.assert_frame_equal(ordered_df, df)
+        logs = self.handler.buffer.getvalue().strip()
+        self.assertEqual(logs, "Empty csv_column_order list provided in config file, "
+                               "returning dataframe with default column order")
 
-        def test_reorder_columns_when_all_column_names_wrong(self):
+    def test_reorder_columns_when_all_column_names_wrong(self):
+        # Arrange list indicating column order (only wrong names inputed) and dataframe for reordering
+        column_names = ['WRONG']
+        data = {
+            'Name': ['Juan', 'Maria', 'Pedro'],
+            'Age': [25, 30, 35]
+        }
+        df = pd.DataFrame(data)
 
-            # Arrange list indicating column order (only wrong names inputed) and dataframe for reordering
-            column_names = ['WRONG']
-            data = {
-                'Name': ['Juan', 'Maria', 'Pedro'],
-                'Age': [25, 30, 35]
-            }
-            df = pd.DataFrame(data)
+        with self.assertRaises(ValueError) as value_error:
+            _reorder_columns(column_names, df)
+            self.assertEqual(str(value_error.exception), "All column names in config file are wrong")
 
-            with self.assertRaises(ValueError) as value_error:
-                _reorder_columns(column_names, df)
-                self.assertEqual(str(value_error.exception), "All column names in config file are wrong")
-            
-            logs = self.handler.buffer.getvalue().strip()
-            self.assertEqual(logs, "'WRONG' specified in config file not is not a column name")
+        logs = self.handler.buffer.getvalue().strip()
+        self.assertEqual(logs, "'WRONG' specified in config file not is not a column name")
 
+    def test_reorder_columns_when_some_column_names_wrong(self):
+        # Arrange list indicating column order (some wrong names inputed) and dataframe for reordering
+        column_names = ['WRONG', 'Name', 'Age']
+        data = {
+            'Name': ['Juan', 'Maria', 'Pedro'],
+            'Age': [25, 30, 35]
+        }
+        df = pd.DataFrame(data)
 
-        def test_reorder_columns_when_some_column_names_wrong(self):
-      
-            # Arrange list indicating column order (some wrong names inputed) and dataframe for reordering
-            column_names = ['WRONG', 'Name', 'Age']
-            data = {
-                'Name': ['Juan', 'Maria', 'Pedro'],
-                'Age': [25, 30, 35]
-            }
-            df = pd.DataFrame(data)
+        # Act (reorder dataframe according to list)
+        ordered_df = _reorder_columns(column_names, df)
 
-            # Act (reorder dataframe according to list)
-            ordered_df = _reorder_columns(column_names, df)
+        pd.testing.assert_frame_equal(ordered_df, df[['Name', 'Age']])
+        logs = self.handler.buffer.getvalue().strip()
+        self.assertEqual(logs, "'WRONG' specified in config file not is not a column name")
 
-            pd.testing.assert_frame_equal(ordered_df, df[['Name', 'Age']])
-            logs = self.handler.buffer.getvalue().strip()
-            self.assertEqual(logs, "'WRONG' specified in config file not is not a column name")
+    def test_reorder_columns_when_some_column_names_missing(self):
+        # Arrange list indicating column order (columns missing) and dataframe for reordering
+        column_names = ['Name']
+        data = {
+            'Name': ['Juan', 'Maria', 'Pedro'],
+            'Age': [25, 30, 35]
+        }
+        df = pd.DataFrame(data)
 
+        # Act (reorder dataframe according to list)
+        ordered_df = _reorder_columns(column_names, df)
 
-        def test_reorder_columns_when_some_column_names_missing(self):
-    
-            # Arrange list indicating column order (columns missing) and dataframe for reordering
-            column_names = ['Name']
-            data = {
-                'Name': ['Juan', 'Maria', 'Pedro'],
-                'Age': [25, 30, 35]
-            }
-            df = pd.DataFrame(data)
+        # Assertion
+        pd.testing.assert_frame_equal(ordered_df, df[['Name']])
 
-            # Act (reorder dataframe according to list)
-            ordered_df = _reorder_columns(column_names, df)
+        logs = self.handler.buffer.getvalue().strip()
+        self.assertEqual(logs, "'Age' column discarded as it is not in config file")
 
-            # Assertion
-            pd.testing.assert_frame_equal(ordered_df, df[['Name']])
+    def test_reorder_columns_success(self):
+        # Arrange list indicating column order and dataframe for reordering
+        column_names = ['Age', 'Name']
+        data = {
+            'Name': ['Juan', 'Maria', 'Pedro'],
+            'Age': [25, 30, 35]
+        }
+        df = pd.DataFrame(data)
 
-            logs = self.handler.buffer.getvalue().strip()
-            self.assertEqual(logs, "'Age' column discarded as it is not in config file")
+        # Act (reorder dataframe according to list)
+        ordered_df = _reorder_columns(column_names, df)
 
+        # Assertion
+        pd.testing.assert_frame_equal(ordered_df, df[['Age', 'Name']])
 
-        def test_reorder_columns_success(self):
+    @patch('primer.write_primer_output.DesignerConfig')
+    def test_export_three_optimal_primers_to_csv(self, mock_designer_config):
+        # Arrange
+        mock_designer_config.return_value.params = {'csv_column_order': ['col1', 'col2', 'col3']}
+        data = {
+            'col1': [1, 2, 3, 4, 5, 6, 7],
+            'col2': ['A', 'B', 'C', 'D', 'E', 'F', 'G'],
+            'col3': ['X', 'Y', 'Z', 'W', 'V', 'U', 'T']
+        }
+        df = pd.DataFrame(data)
 
-            # Arrange list indicating column order and dataframe for reordering
-            column_names = ['Age', 'Name']
-            data = {
-                'Name': ['Juan', 'Maria', 'Pedro'],
-                'Age': [25, 30, 35]
-            }
-            df = pd.DataFrame(data)
+        export_dir = '/mock/directory'
+        self.fs.create_dir(export_dir)
 
-            # Act (reorder dataframe according to list)
-            ordered_df = _reorder_columns(column_names, df)
+        # Act
+        result_path = export_three_optimal_primer_pairs_to_csv(df, export_dir)
 
-            # Assertion
-            pd.testing.assert_frame_equal(ordered_df, df[['Age', 'Name']])
+        # Assert
+        expected_path = path.join(export_dir, 'optimal_primer_pairs.csv')
+        self.assertEqual(result_path, expected_path)
+
+        with open(result_path, 'r') as file:
+            content = file.read()
+
+        expected_content = "col1,col2,col3\n1,A,X\n2,B,Y\n3,C,Z\n4,D,W\n5,E,V\n6,F,U\n"
+
+        self.assertEqual(content, expected_content)
 
 
 class TestDataFrameBuild(TestCase):
