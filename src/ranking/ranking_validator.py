2dd005b0d86c6adbdf57a1a1168951d4cc34868e
Shriniwas <sk34@sanger.ac.uk>
2024-06-06 10:54:19 +0000

    Remove unwanted code

--- src/ranking/ranking_validator.py
+++ src/ranking/ranking_validator.py
@@ -41,33 +41,3 @@ def _get_rank_criterion(name: str) -> RankingCriteria:
 def _check_criterion_name(name: str):
     if name not in [_criterion.name for _criterion in RankingCriteria.__subclasses__()]:
         raise ValueError(f'Invalid ranking criteria: the given rank criterion "{name}" is not valid')
-
-
-#         for criteria in self.ranking_columns:
-#             created = RankingCriteria.from_dict(criteria)
-#             self.created_criteria.append(created)
-
-#         validated, column_name, validated_rankers = validate_ranking_columns(self.created_criteria, self.csv_columns)
-
-#         if not validated:
-#             logger.warning(f"Invalid ranking column name {column_name}. Please Check configuration file.")
-#             raise Exception(f"Invalid ranking column name {column_name}. Please Check configuration file.")
-        
-#         self.created_criteria = validated_rankers
-    
-
-# def validate_ranking_columns(created_criteria, csv_columns):
-
-#     is_validated = True
-#     column_name = ""
-#     validated_rankers = []
-
-#     for ranking_criteria in created_criteria:
-#         if not ranking_criteria.column in csv_columns:
-#             is_validated = not is_validated
-#             column_name = ranking_criteria.column
-#             return is_validated, column_name, validated_rankers
-        
-#         validated_rankers.append(ranking_criteria)
-
-#     return is_validated, validated_rankers, validated_rankers
