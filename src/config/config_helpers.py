1eb5fe831ea97be64e155f538f25792974f4deb6
Miguel Rada <mr33@sanger.ac.uk>
2026-01-12 09:26:48 +0000

    Code review changes: remove unused logger in config_helper

--- src/config/config_helpers.py
+++ src/config/config_helpers.py
@@ -1,9 +1,5 @@
-from custom_logger.custom_logger import CustomLogger
-
 import sys
 
-# Initialize logger
-logger = CustomLogger(__name__)
 
 def check_non_negative_integer(name, value):
     if not isinstance(value, int) or value < 0:
