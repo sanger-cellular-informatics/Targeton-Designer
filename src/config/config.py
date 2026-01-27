951934be23dba2b4770c52e18c47544c67c815d7
sk34 <sk34@sanger.ac.uk>
2026-01-27 17:35:08 +0000

    Fix path relative paths to config files

--- src/config/config.py
+++ src/config/config.py
@@ -1,19 +1,34 @@
+from math import log
 from config.config_helpers import check_non_negative_integer
 from config.ipcress_params import IpcressParameters
 from utils.file_system import parse_json
 
 import sys
 from custom_logger.custom_logger import CustomLogger
+import os
+from pathlib import Path
 
 # Initialize logger
 logger = CustomLogger(__name__)
 
 
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+DEFAULT_CONFIG_PATH = BASE_DIR / "config" / "default_designer.config.json"
+DEFAULT_PRIMER3_CONFIG_PATH = BASE_DIR / "config" / "default_primer3.config.json"
+
 class DesignerConfig:
     def __init__(self, args: dict):
-        default_config_path = 'config/default_designer.config.json'
 
-        config = DesignerConfig.read_config(default_config_path, args.get('conf', None))
+
+        if not args.get('conf'):
+            d_conf = DEFAULT_CONFIG_PATH
+        else:
+            d_conf = args.get('conf')
+
+        logger.info(f"Base directory for configuration: {BASE_DIR}")
+        logger.info(f"Using configuration file: {d_conf}")
+
+        config = DesignerConfig.read_config(d_conf)
 
         # Check if filters exist in configuration.
         if not config.get("filters"):
@@ -49,8 +64,14 @@ class DesignerConfig:
         self.prefix_output_dir = args.get('dir', None) or config.get('dir', None)
         self.fasta = args.get('fasta', None) or config.get('fasta', None)
 
-        primer3_params_path = (args.get('primer3_params', None) or config.get('primer3_params', None)
-                               or 'config/default_primer3.config.json')
+        # Use provided primer3_params path from args, otherwise use default primer3 config path
+        if not args.get('primer3_params'):
+            primer3_params_path = DEFAULT_PRIMER3_CONFIG_PATH
+        else:
+            primer3_params_path = args.get('primer3_params')
+
+        logger.info(f"Using primer3 parameters file: {primer3_params_path}")
+
         self.primer3_params = parse_json(primer3_params_path)
 
     @staticmethod
