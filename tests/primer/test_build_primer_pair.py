7021731618028be932d8d2dcc38b8cc459144825
Miguel Rada <mr33@sanger.ac.uk>
2026-03-16 20:36:16 +0000

    Make readable build primer pairs tests

---
+++ tests/primer/test_build_primer_pair.py
@@ -0,0 +1,150 @@
+import unittest
+
+from pyfakefs.fake_filesystem_unittest import TestCase
+from parameterized import parameterized
+from unittest.mock import patch, Mock
+
+from primer.designed_primer import Interval, DesignedPrimer
+from primer.primer_pair import PrimerPair
+from primer.slice_data import SliceData
+from primer.build_primer_pairs import _name_primers, _calculate_primer_coords, build_primer_pairs
+
+
+class TestPrimerPairNamePrimers(TestCase):
+    @parameterized.expand([
+        ('left', '+', 'LibAmpF'),
+        ('left', '-', 'LibAmpR'),
+        ('right', '+', 'LibAmpR'),
+        ('right', '-', 'LibAmpF'),
+    ])
+    def test_name_primers(self, test_input, strand, expected):
+        # act
+        actual = _name_primers(test_input, strand)
+
+        # assert
+        self.assertEqual(actual, expected)
+
+
+class TestBuildPrimerPair(TestCase):
+
+    def test_build_primer_pairs_when_no_primer_pairs_found(self):
+        designs = {
+            'PRIMER_LEFT': [],
+            'PRIMER_PAIR': [],
+            'PRIMER_RIGHT': []
+        }
+
+        result = build_primer_pairs(design=designs, slice_data=Mock(), stringency=0.5)
+
+        self.assertEqual(result, [])
+
+    def test_build_primer_pairs_when_primer_pairs_found(self):
+        slice_data = SliceData(
+            name="AABB",
+            start=50398701,
+            end=50399203,
+            strand="-",
+            chromosome="19",
+            bases="CAGGCTGCACGGGGTGGAAGGGCAGTCCTTTGTAGGATTCAAAATTGGCCAGAGTGGGGTCACAGGTCAGCTTTGCAGCTGACTCTTG"
+                  "GACCAAAGGCAAGACCAGGGGCAATGGGTTGGGCAGCAGGAACCTCCAACTCCAAGCCTTACCGTCTGCAACCCCCTCCAGGACTGAC"
+                  "TGCAGCTCCTCCTCCTCCTGCTCCTGCAGCCTGTGTTCTGCCTCCATCTCCTCCATCAGTGCCAGGTCCTCCTCGAATTGGGATGGCC"
+                  "GAGGTGCATCATCATCATCCCAGAGGCCCCCACGGGCCCGCTTTGGGGGCACCCCGGGCCCTGGGCCTGGCCGCCGCTTGCCATCCAT"
+                  "CCTGCTGGGCAAGTTGGAGCTTGGTGGAGGTTCTGACCGGAGACACCTCTTACCCTGTCTCCATGGCATCCCATGCACCCACCATGAC"
+                  "CACGAGCCTTTTCCTACTCTGGACTGGCTGTACTAACATAGCTCTCTGCACCCTGCAGGCAGT",
+            flanking_region=150,
+            exclusion_region=5
+        )
+        design = {
+            "PRIMER_PAIR": [
+                {"PENALTY": 0.3122182485828068, "COMPL_ANY_TH": 6.098089414466301, "COMPL_END_TH": 2.0852502864723874,
+                 "PRODUCT_SIZE": 279, "PRODUCT_TM": 92.61358067328065},
+                {"PENALTY": 0.3125104180142119, "COMPL_ANY_TH": 6.098089414466301, "COMPL_END_TH": 2.0852502864723874,
+                 "PRODUCT_SIZE": 280, "PRODUCT_TM": 92.52731595233853}],
+            "PRIMER_LEFT": [
+                {"PENALTY": 0.16915001782036365, "SEQUENCE": "GCAGGAACCTCCAACTCCAA", "COORDS": [123, 20],
+                 "TM": 59.88951398263703, "GC_PERCENT": 55.0, "SELF_ANY_TH": 0.0, "SELF_END_TH": 0.0,
+                 "HAIRPIN_TH": 39.610015792775926, "END_STABILITY": 3.53},
+                {"PENALTY": 0.16915001782036365, "SEQUENCE": "GCAGGAACCTCCAACTCCAA", "COORDS": [123, 20],
+                 "TM": 59.88951398263703, "GC_PERCENT": 55.0, "SELF_ANY_TH": 0.0, "SELF_END_TH": 0.0,
+                 "HAIRPIN_TH": 39.610015792775926, "END_STABILITY": 3.53}],
+            "PRIMER_RIGHT": [
+                {"PENALTY": 0.14306823076244313, "SEQUENCE": "AGAGGTGTCTCCGGTCAGAA", "COORDS": [401, 20],
+                 "TM": 59.887393730886686, "GC_PERCENT": 55.0, "SELF_ANY_TH": 0.0, "SELF_END_TH": 0.0,
+                 "HAIRPIN_TH": 40.83258802708207, "END_STABILITY": 3.02},
+                {"PENALTY": 0.14336040019384827, "SEQUENCE": "AAGAGGTGTCTCCGGTCAGA", "COORDS": [402, 20],
+                 "TM": 59.887393730886686, "GC_PERCENT": 55.0, "SELF_ANY_TH": 0.0, "SELF_END_TH": 0.0,
+                 "HAIRPIN_TH": 40.83258802708207, "END_STABILITY": 3.27}]
+        }
+
+        result = build_primer_pairs(design=design, slice_data=slice_data, stringency=1)
+
+        primer1 = PrimerPair(pair_id='AABB_LibAmp_0_str1', uid='9f6b0fea-1e71-11f1-8d25-9aac72ca3ecb', chromosome='19',
+                             pre_targeton_start=50398701, pre_targeton_end=50399203, product_size=279,
+                             stringency=1, targeton_id='AABB'
+                             )
+        forward1 = DesignedPrimer(name='AABB_LibAmpF_0', penalty=0.14306823076244313,
+                                  pair_id='AABB_LibAmp_0_str1', sequence='AGAGGTGTCTCCGGTCAGAA',
+                                  coords=Interval(start=401, end=20), primer_start=50398802,
+                                  primer_end=50398821, strand='+', tm=59.887393730886686,
+                                  gc_percent=55.0, self_any_th=0.0, self_end_th=0.0,
+                                  hairpin_th=40.83258802708207, end_stability=3.02)
+        reverse1 = DesignedPrimer(name='AABB_LibAmpR_0', penalty=0.16915001782036365,
+                                  pair_id='AABB_LibAmp_0_str1', sequence='GCAGGAACCTCCAACTCCAA',
+                                  coords=Interval(start=123, end=20), primer_start=50399061,
+                                  primer_end=50399080, strand='-', tm=59.88951398263703,
+                                  gc_percent=55.0, self_any_th=0.0, self_end_th=0.0,
+                                  hairpin_th=39.610015792775926, end_stability=3.53)
+
+        primer2 = PrimerPair(pair_id='AABB_LibAmp_1_str1', uid='9f6b1986-1e71-11f1-8d25-9aac72ca3ecb', chromosome='19',
+                             pre_targeton_start=50398701, pre_targeton_end=50399203, product_size=280, stringency=1,
+                             targeton_id='AABB',
+                             )
+        forward2 = DesignedPrimer(name='AABB_LibAmpF_1', penalty=0.14336040019384827,
+                                  pair_id='AABB_LibAmp_1_str1',
+                                  sequence='AAGAGGTGTCTCCGGTCAGA',
+                                  coords=Interval(start=402, end=20),
+                                  primer_start=50398801, primer_end=50398820,
+                                  strand='+', tm=59.887393730886686, gc_percent=55.0,
+                                  self_any_th=0.0, self_end_th=0.0,
+                                  hairpin_th=40.83258802708207, end_stability=3.27)
+        reverse2 = DesignedPrimer(name='AABB_LibAmpR_1', penalty=0.16915001782036365,
+                                  pair_id='AABB_LibAmp_1_str1', sequence='GCAGGAACCTCCAACTCCAA',
+                                  coords=Interval(start=123, end=20), primer_start=50399061,
+                                  primer_end=50399080, strand='-', tm=59.88951398263703, gc_percent=55.0,
+                                  self_any_th=0.0, self_end_th=0.0, hairpin_th=39.610015792775926,
+                                  end_stability=3.53)
+        primer1.forward = forward1
+        primer1.reverse = reverse1
+        primer2.forward = forward2
+        primer2.reverse = reverse2
+
+        self.assertEqual(len(result), 2)
+        self.assertEqual(result[0], primer1)
+        self.assertEqual(result[1], primer2)
+
+
+class TestCalculatePrimerCoords(unittest.TestCase):
+
+    @parameterized.expand(
+        [
+            ('left', [55, 20], 44490254, 44490755, '+', (44490309, 44490328)),
+            ('right', [168, 19], 44490254, 44490755, '+', (44490404, 44490422)),
+            ('left', [39, 20], 44490254, 44490755, '-', (44490697, 44490716)),
+            ('right', [167, 19], 44490254, 44490755, '-', (44490588, 44490606)),
+        ])
+    def test_calculate_primer_coords(
+            self,
+            side,
+            coords,
+            slice_start,
+            slice_end,
+            strand,
+            expected_result
+    ):
+        result = _calculate_primer_coords(side, coords, slice_start, slice_end, strand)
+
+        self.assertEqual(result, expected_result)
+
+
+if __name__ == '__main__':
+    unittest.main()
